032a1aa27d4d4ab5e22515f7e54c364d34c905a0
pulipach <c60569@outlook.com>
2020-03-23 12:10:55 +0700

    pulipach

--- app.js
+++ app.js
@@ -19,36 +19,36 @@ var server =http.createServer(function(req,res){
   myReadStream.pipe(res);
 
   //css page1
-  if (req.url === '/style.css') {
+  if (req.url === 'css/style.css') {
     res.writeHead(200, {'Content-type' : 'text/css'});
-    var fileContents = fs.readFileSync('style.css', {encoding: 'utf8'});
+    var fileContents = fs.readFileSync('css/style.css', {encoding: 'utf8'});
     
     res.write(fileContents);
   }
   //css page2
-  if (req.url === '/animate.css') {
+  if (req.url === 'css/animate.css') {
     res.writeHead(200, {'Content-type' : 'text/css'});
-    var fileContents = fs.readFileSync('animate.css', {encoding: 'utf8'});
+    var fileContents = fs.readFileSync('css/animate.css', {encoding: 'utf8'});
     res.write(fileContents);
   }
-  if (req.url === '/bootstrap.css') {
+  if (req.url === 'css/bootstrap.css') {
     res.writeHead(200, {'Content-type' : 'text/css'});
-    var fileContents = fs.readFileSync('bootstrap.css', {encoding: 'utf8'});
+    var fileContents = fs.readFileSync('css/bootstrap.css', {encoding: 'utf8'});
     res.write(fileContents);
   }
-  if (req.url === '/flexslider.css') {
+  if (req.url === 'css/flexslider.css') {
     res.writeHead(200, {'Content-type' : 'text/css'});
-    var fileContents = fs.readFileSync('flexslider.css', {encoding: 'utf8'});
+    var fileContents = fs.readFileSync('css/flexslider.css', {encoding: 'utf8'});
     res.write(fileContents);
   }
-  if (req.url === '/icomoon.css') {
+  if (req.url === 'css/icomoon.css') {
     res.writeHead(200, {'Content-type' : 'text/css'});
-    var fileContents = fs.readFileSync('icomoon.css', {encoding: 'utf8'});
+    var fileContents = fs.readFileSync('css/icomoon.css', {encoding: 'utf8'});
     res.write(fileContents);
   }
-  if (req.url === '/style2.css') {
+  if (req.url === 'css/style2.css') {
     res.writeHead(200, {'Content-type' : 'text/css'});
-    var fileContents = fs.readFileSync('style2.css', {encoding: 'utf8'});
+    var fileContents = fs.readFileSync('css/style2.css', {encoding: 'utf8'});
     res.write(fileContents);
   }
   
