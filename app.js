83164231c201eb448e114c7dcfa87befaf2ccfa9
khunuch <c60329@outlook.com>
2020-03-28 13:45:58 +0700

    63:change and connect ejs,css

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dataRouter = require('./routes/data');
+var newsRouter = require('./routes/news');
+var stayRouter = require('./routes/stay');
 
 var app = express();
 
@@ -27,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/data', dataRouter);
+app.use('/news', newsRouter);
+app.use('/stay', stayRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
