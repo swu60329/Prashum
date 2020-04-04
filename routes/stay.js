0a17192add4fd00b2680621c2d2e358723abaa30
chama <c60562@outlook.com>
2020-04-04 13:44:38 +0700

    delete all of stay

--- routes/stay.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET data page. */
-router.get('/', function(req, res, next) {
-  res.render('stay');
-});
-
-module.exports = router;
