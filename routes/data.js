3cffb1becfe9f6f54e0b43a51553a81de1850528
pulipach <c60569@outlook.com>
2020-04-23 22:06:48 +0700

    i think it's final.

--- routes/data.js
+++ routes/data.js
@@ -7,16 +7,16 @@ var cnt = new cnt_b();
 
 /* GET data page. */
 router.get('/', function(req, res, next) {
+  console.time("settime");
   var n_vst = cnt.getCount_b();
   cnt.setCnt_blog(n_vst);
   var test = cnt.getCount_b();
   cnt.addPage_blog(test);
   var test2 = cnt.getCount_b()
 
-
-
   // cnt.addPage(n_vst);
   console.log(test2);
+  console.timeEnd("settime");
 
   res.render('data');
 });
--- routes/news.js
+++ routes/news.js
@@ -6,17 +6,16 @@ var cnt = new cnt_n();
 
 /* GET data page. */
 router.get('/', function(req, res, next) {
+  console.time("settime");
   var n_vst = cnt.getCount_n();
   cnt.setCnt_news(n_vst);
   var test = cnt.getCount_n();
   cnt.addPage_news(test);
   var test2 = cnt.getCount_n()
 
-
-
   // cnt.addPage(n_vst);
   console.log(test2);
-
+  console.timeEnd("settime");
   res.render('news');
 });
 
